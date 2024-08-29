0bd76c130fccb7006034204fbb38f64b344b75ac
kthurm <karalthurmond@gmail.com>
2024-08-29 13:47:26 -0400

    Creates resuable api vehicle  hook

---
+++ src/components/hooks/UseAxios.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Vehicle {
+  timestamp: string;
+  classification: string;
+  axles: number;
+  height: number;
+}
+
+const client = axios.create({
+  baseURL: "http://44.222.172.90:8080",
+});
+
+const useFetchData = () => {
+  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    return date.toUTCString();
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.get<Vehicle[]>("/getData");
+        console.log("Vehicles Fetched");
+        setVehicles(response.data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return {
+    vehicles,
+    loading,
+    formatDate,
+  };
+};
+
+export default useFetchData;
