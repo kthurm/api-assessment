0e38d8c7fe05505868c49ef5e621fba072604497
kthurm <karalthurmond@gmail.com>
2024-08-30 13:48:11 -0400

    Adds google font and color configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+
     extend: {
       colors: {
         light: "#ffa600",
